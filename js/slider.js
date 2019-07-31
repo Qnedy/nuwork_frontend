26738d29604adabd7fb90c54f18024e34d0df0b4
qnedy <altmaisefequatro@gmail.com>
2019-07-31 19:22:15 -0300

    modal_And_Nav_Topo_Ok

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,9 @@
 var intervalo = 3000;
 
-/*function slide1(){
-    document.getElementById("slidee").src="../img/eletricista.png";
-    setTimeout("slide2()", intervalo);
-}
-
-function slide2(){
-    document.getElementById("slidee").src="../img/third3.png";
-    setTimeout("slide3()", intervalo);
-}
-
-function slide3(){
-    document.getElementById("slidee").src="../img/encanador.png";
-    setTimeout("slide1()", intervalo);
-}*/
-
 function muda1(){
     document.getElementById("main_home").style.width = '100%';
     document.getElementById("main_home").style.height = '400px';
     document.getElementById("main_home").style.marginTop = '95px';
-    
     document.getElementById("main_home").style.background = 'url("https://i.imgur.com/xe91xqk.png") no-repeat center';
     document.getElementById("main_home").style.backgroundSize = 'cover';
     setTimeout("muda2()", intervalo)
@@ -29,7 +13,6 @@ function muda2(){
     document.getElementById("main_home").style.width = '100%';
     document.getElementById("main_home").style.height = '400px';
     document.getElementById("main_home").style.marginTop = '95px';
-    
     document.getElementById("main_home").style.background = 'url("https://i.imgur.com/W2SdiGo.png") no-repeat center';
     document.getElementById("main_home").style.backgroundSize = 'cover';
     setTimeout("muda3()", intervalo)
@@ -39,8 +22,46 @@ function muda3(){
     document.getElementById("main_home").style.width = '100%';
     document.getElementById("main_home").style.height = '400px';
     document.getElementById("main_home").style.marginTop = '95px';
-    
     document.getElementById("main_home").style.background = 'url("https://i.imgur.com/3fEqJON.png") no-repeat center';
     document.getElementById("main_home").style.backgroundSize = 'cover';
     setTimeout("muda1()", intervalo)
 }
+
+
+function navTopo(navId){
+    const modal = document.getElementById(navId);
+    modal.classList.add('introducaoTopo');
+}
+
+function navNormal(navId){
+    const modal = document.getElementById(navId);
+    modal.classList.remove('introducaoTopo');
+}
+
+document.addEventListener('scroll', function(){
+    if(window.pageYOffset > 50)
+        navTopo('nav_id');
+
+    if(window.pageYOffset <= 100)
+        navNormal('nav_id');
+}, false);
+
+/* modal */
+
+function mostraModal(idModal){
+    const modal = document.getElementById(idModal);
+    modal.classList.add('mostrar');
+}
+
+function removerModal(idModal){
+    const modal = document.getElementById(idModal);
+    modal.classList.remove('mostrar');
+}
+
+document.addEventListener('scroll', function(){
+    if(window.pageYOffset > 90)
+        mostraModal('modal_id');
+
+    if(window.pageYOffset <= 100)
+        removerModal('modal_id');
+}, false);
