597a15838ab365f503d4370e2ed34fa9c060c07c
qnedy <altmaisefequatro@gmail.com>
2019-08-15 18:01:45 -0300

    addBuyPage

--- js/admin.js
+++ js/admin.js
@@ -14,3 +14,22 @@ iniciaModal();
 
 const botaoFechar = document.querySelector('[data-js="btn_fecharModal"]');
 botaoFechar.addEventListener('click', fechaModal, false)
+
+
+
+function abreModalCompra(){
+    const modalCompra = document.querySelector('[data-js="modal_compra"]');
+    modalCompra.classList.add("mostrar_compra");
+    
+}
+
+function fechaModalCompra(){
+    const modalCompra = document.querySelector('[data-js="modal_compra"]');
+    modalCompra.classList.remove("mostrar_compra");
+}
+
+const botaoAbreCompra = document.querySelector('[data-js="btn_comprarMoeda"]');
+botaoAbreCompra.addEventListener('click', abreModalCompra, false)
+
+const botaoFecharCompra = document.querySelector('[data-js="btn_fecharModalCompra"]');
+botaoFecharCompra.addEventListener('click', fechaModalCompra, false)
